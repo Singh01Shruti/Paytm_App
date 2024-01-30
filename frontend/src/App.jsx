c08499bdc1050f0b918e744b47db17f969f6281b
Shruti Singh <singh001shruti@gmail.com>
2024-01-30 21:12:26 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp"
 import Dashboard from "./components/Dashboard"
+import Transfer from "./components/Transfer"
 import { RecoilRoot } from "recoil"
+import { Suspense } from "react"
+
+
 function App() {
   return (
     <>
     <RecoilRoot>
     <BrowserRouter>
     <Routes>
-    <Route path = "/" element = {<SignUp />}></Route>
-    <Route path = "/signin" element = {<SignIn />}></Route>
-    <Route path = "/dashboard" element = {<Dashboard />}></Route>
+    <Route path = "/" element = {(<Suspense fallback = "loading..."><SignUp /></Suspense>)}/>
+    <Route path = "/signin" element = {(<Suspense fallback = "loading..."><SignIn /></Suspense>)}/>
+    <Route path = "/dashboard" element = {(<Suspense fallback = "loading..."><Dashboard /></Suspense>)}/>
+    <Route path = "/transfer" element = {(<Suspense fallback = "loading..."><Transfer /></Suspense>)}/>
     </Routes>
     </BrowserRouter>
+
     </RecoilRoot>
     </>
   )
 }
+
 
 export default App
